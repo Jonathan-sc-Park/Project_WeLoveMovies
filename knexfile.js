9428f2fff66138d3e5097d9bdf57d49acf6f8057
Jonathan Park <110662940+Jonathan-sc-Park@users.noreply.github.com>
2022-12-05 14:33:31 -0800

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -9,7 +9,7 @@ const {
 module.exports = { 
   development: {
     client: "postgresql",
-    connection: URL,
+    connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -21,7 +21,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: URL,
+    connection: DATABASE_URL,
     pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
